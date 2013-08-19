d62ec4a575e3e9643835172e04def7dc2133d095
Count Derp <thecount@moehae.com>
2013-08-18 21:11:32 -0500

    added initial wrapper stuff

---
+++ README.md
@@ -0,0 +1,13 @@
+# mohae-nginx/ cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: mohae-nginx/
+# Recipe:: default
+#
+# Copyright (C) 2013 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
+include_recipe 'nginx'
